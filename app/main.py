886e5b1ee41688e9146cada187b85947c69138ad
Salvador Rueda <salvador.rueda@gmail.com>
2026-03-31 11:11:11 +0200

    Ajusta la configuració d'arrencada i del contenidor

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 import logging
+import sys
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
-)
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from app.database import create_db
 from app.routers import personas, relaciones, exportar, interpretar
+
+
+def _setup_logging() -> None:
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter("%(asctime)s [%(name)s] %(levelname)s: %(message)s"))
+    logger = logging.getLogger("fac")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    logger.propagate = False
+
+
+_setup_logging()
 
 
 @asynccontextmanager
